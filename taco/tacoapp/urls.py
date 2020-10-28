01712d62d83936d559abd70e1d92db08762f0243
patthrasher <patrickalanthrasher@gmail.com>
2020-10-27 22:28:19 -0500

    added and edited files

--- taco/tacoapp/urls.py
+++ taco/tacoapp/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from tacoapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
 ]
--- taco/tacoapp/views.py
+++ taco/tacoapp/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import FoodForm
 
 def index(request) :
-    return HttpResponse('From memory baby')
-# Create your views here.
+    if request.method == 'POST' :
+        form = FoodForm(request.POST or None)
+
+        if form.is_valid() :
+            form.save()
+            all_items = Food.objects.all
+            return render(request, 'tacoapp/index.html', {'all_items' : 'something to display'})
+
+    else :
+        return render(request, 'tacoapp/index.html', {'all_items' : 'nothing to display'})
