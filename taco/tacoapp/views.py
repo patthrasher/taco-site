59ae31c360d14625afca9e8bb8cd0059e54521b7
patthrasher <patrickalanthrasher@gmail.com>
2020-10-30 12:28:45 -0500

    edited date format, added messages to views.py, bootstrap css index.html

--- taco/tacoapp/views.py
+++ taco/tacoapp/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from .models import Food
 from .forms import FoodForm
+from django.contrib import messages
+import datetime
 
 def index(request) :
+
+    today = str(datetime.date.today())
+
     if request.method == 'POST' :
         form = FoodForm(request.POST or None)
 
         if form.is_valid() :
             form.save()
             all_items = Food.objects.all
-            return render(request, 'tacoapp/index.html', {'all_items' : all_items})
+            context = {'all_items' : all_items,
+                'today' : today}
+            messages.success(request, ('Item has been logged'))
+            return render(request, 'tacoapp/index.html', context)
 
     else :
         all_items = Food.objects.all
-        return render(request, 'tacoapp/index.html', {'all_items' : all_items})
+        context = {'all_items' : all_items,
+            'today' : today}
+        return render(request, 'tacoapp/index.html', context)
