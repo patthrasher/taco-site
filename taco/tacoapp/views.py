b4567b0ecff8f130cbe5c4db0ab6321ca4cb263d
patthrasher <patrickalanthrasher@gmail.com>
2020-11-05 15:52:27 -0600

    edited files, started calculations

--- taco/tacoapp/views.py
+++ taco/tacoapp/views.py
@@ -17,21 +17,49 @@ def index(request) :
 
         if form.is_valid() :
             form.save()
-            all_items = Food.objects.all    
+            all_items = Food.objects.all
             messages.success(request, ('Item has been logged'))
             return redirect('tacoapp:index')
     else :
-
+        context = {
+            'today' : today,
+        }
         return render(request, 'tacoapp/index.html', context)
 
 @login_required
 def manager(request) :
+
+    total = 0
 
     if not request.user.username == 'sarah' :
         messages.success(request, ('You sir do not have access to that page!'))
         return redirect('tacoapp:index')
 
     else :
-        all_items = Food.objects.all
-        context = {'all_items' : all_items}
+        all_items = Food.objects.all()
+
+        for each in all_items :
+            total = total + each.number
+
+        weekdays = ('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday')
+
+        lst = []
+        for each in all_items :
+            day = weekdays[each.date.weekday()]
+            # dayday = day.weekday()
+            # daystring = weekDays[day]
+            lst.append(day)
+
+        dc = {}
+        for each in weekdays :
+            # count = lst.count(each)
+            dc.update({each : lst.count(each)})
+
+        context = {
+            'all_items' : all_items,
+            'total' : total,
+            'list' : lst,
+            'dc' : dc,
+        }
+
         return render(request, 'tacoapp/manager.html', context)
